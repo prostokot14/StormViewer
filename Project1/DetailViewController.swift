d488a8f3de14320842ff5549071e8a205e77ce55
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-05-22 12:52:40 +0200

    Project 1

---
+++ Project1/DetailViewController.swift
@@ -0,0 +1,40 @@
+//
+//  DetailViewController.swift
+//  Project1
+//
+//  Created by Антон Кашников on 19.02.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    var selectedPictureNumber = 0
+    var totalPictures = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Picture \(selectedPictureNumber) of \(totalPictures)"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        navigationController?.hidesBarsOnTap = false
+    }
+}
