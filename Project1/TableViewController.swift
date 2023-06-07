1e2b66c3f13195ee7fffe701c3ee632ceb7b4e3b
prostokot14 <86567361+prostokot14@users.noreply.github.com>
2023-06-07 14:26:05 +0200

    Improved code

--- Project1/TableViewController.swift
+++ Project1/TableViewController.swift
@@ -21,11 +21,11 @@ final class TableViewController: UITableViewController {
         
         let fm = FileManager.default
         let path = Bundle.main.resourcePath!
-        let items = try! fm.contentsOfDirectory(atPath: path) // in this case we got the path straight from iOS – we didn’t type it in by hand, so if reading from our own app’s bundle doesn’t work then clearly something is very wrong indeed, so that's because we use "try!"
+        let items = try! fm.contentsOfDirectory(atPath: path)
 
-        for item in items {
-            if item.hasPrefix("nssl") {
-                pictures.append(item)
+        for picture in pictures {
+            if picture.hasPrefix("nssl") {
+                pictures.append(picture)
             }
         }
         
@@ -39,32 +39,30 @@ final class TableViewController: UITableViewController {
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
-        cell.textLabel?.text = pictures[indexPath.row]
+
+        if #available(iOS 14.0, *) {
+            var content = cell.defaultContentConfiguration()
+            content.text = pictures[indexPath.row]
+            cell.contentConfiguration = content
+        } else {
+            cell.textLabel?.text = pictures[indexPath.row]
+        }
+
         return cell
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // 1: try loading the "Detail" view controller and typecasting it to be DetailViewController
-        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
-            // 2: success! Set its selectedImage property
-            vc.selectedImage = pictures[indexPath.row]
-            vc.selectedPictureNumber = indexPath.row + 1
-            vc.totalPictures = pictures.count
-            // 3: now push it onto the navigation controller
-            navigationController?.pushViewController(vc, animated: true)
+        if let detailViewController = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            detailViewController.selectedImage = pictures[indexPath.row]
+            detailViewController.selectedPictureNumber = indexPath.row + 1
+            detailViewController.totalPictures = pictures.count
+            navigationController?.pushViewController(detailViewController, animated: true)
         }
     }
 
     // MARK: - Private Methods
     @objc private func shareTapped() {
         let activityVC = UIActivityViewController(activityItems: ["Hello! This is great app! Try it now!"], applicationActivities: nil)
-
-        if #available(iOS 16.0, *) {
-            activityVC.popoverPresentationController?.sourceItem = navigationItem.rightBarButtonItem
-        } else {
-            activityVC.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
-        }
-
         present(activityVC, animated: true)
     }
 }
